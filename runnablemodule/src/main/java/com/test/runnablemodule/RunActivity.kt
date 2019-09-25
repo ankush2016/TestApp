68ca618962b932d216f4596712cc0a975a17afc9
ankushkapoor <ankushkapoor2015@gmail.com>
2019-09-25 23:26:05 +0530

    v3

---
+++ runnablemodule/src/main/java/com/test/runnablemodule/RunActivity.kt
@@ -0,0 +1,12 @@
+package com.test.runnablemodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RunActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_run)
+    }
+}
